950568b4588932c578e2b6dcf43d8e6db3cbea2b
YAMAGUCHI Seiji <valda@underscore.jp>
2011-11-25 15:37:45 +0900

    バッファのcoding-systemをもとにmagic commentをつける。

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -726,6 +726,28 @@ and source-file directory for your debugger.")
              (define-key ruby-mode-map "\C-cd" 'rubydb)
              (setq dabbrev-abbrev-skip-leading-regexp "[:@]")))
 
+(defun ruby-insert-magic-comment-if-needed ()
+  "バッファのcoding-systemをもとにmagic commentをつける。"
+  (when (and (eq major-mode 'ruby-mode)
+             (find-multibyte-characters (point-min) (point-max) 1))
+    (save-excursion
+      (goto-char 1)
+      (when (looking-at "^#!")
+        (forward-line 1))
+      (if (re-search-forward "^#.+coding" (point-at-eol) t)
+          (delete-region (point-at-bol) (point-at-eol))
+        (open-line 1))
+      (let* ((coding-system (symbol-name buffer-file-coding-system))
+             (encoding (cond ((string-match "japanese-iso-8bit\\|euc-j" coding-system)
+                              "euc-jp")
+                             ((string-match "shift.jis\\|sjis\\|cp932" coding-system)
+                              "shift_jis")
+                             ((string-match "utf-8" coding-system)
+                              "utf-8"))))
+        (insert (format "# -*- coding: %s -*-" encoding))))))
+
+(add-hook 'before-save-hook 'ruby-insert-magic-comment-if-needed)
+
 ;;; rd-mode
 (when (locate-library "rd-mode")
   (autoload 'rd-mode "rd-mode" "major mode for ruby document formatter RD" t)
