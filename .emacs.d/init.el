10559e71acee03bc2bee731f8bb1ef8459edb1d5
YAMAGUCHI Seiji <valda@underscore.jp>
2012-07-12 19:05:18 +0900

    ruby-mode のインデントをいい感じにする

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -830,6 +830,23 @@ and source-file directory for your debugger.")
              (define-key ruby-mode-map "\C-cd" 'rubydb)
              (setq dabbrev-abbrev-skip-leading-regexp "[:@]")))
 
+;;; ruby-mode のインデントをいい感じにする
+(setq ruby-deep-indent-paren-style nil)
+(defadvice ruby-indent-line (after unindent-closing-paren activate)
+  (let ((column (current-column))
+        indent offset)
+    (save-excursion
+      (back-to-indentation)
+      (let ((state (syntax-ppss)))
+        (setq offset (- column (current-column)))
+        (when (and (eq (char-after) ?\))
+                   (not (zerop (car state))))
+          (goto-char (cadr state))
+          (setq indent (current-indentation)))))
+    (when indent
+      (indent-line-to indent)
+      (when (> offset 0) (forward-char offset)))))
+
 (defun ruby-insert-magic-comment-if-needed ()
   "バッファのcoding-systemをもとにmagic commentをつける。"
   (when (and (eq major-mode 'ruby-mode)
