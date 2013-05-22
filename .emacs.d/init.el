41ab3f600f7ab6d6c52a16f62183135c43514f50
YAMAGUCHI Seiji <valda@underscore.jp>
2013-05-22 14:52:07 +0900

    flymake と font

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1069,24 +1069,21 @@ Highlight last expanded string."
 ;;; ----------------------------------------------------------------------
 ;;; flymake
 ;;; ----------------------------------------------------------------------
-;;(set-face-background 'flymake-errline "red4")
-;;(set-face-background 'flymake-warnline "dark slate blue")
 (when (require 'flymake nil t)
-  ;; rails-mode で require されるのでコメントアウト
-  ;; (defun flymake-ruby-init ()
-  ;;   (let* ((temp-file   (flymake-init-create-temp-buffer-copy
-  ;;                        'flymake-create-temp-inplace))
-  ;;          (local-file  (file-relative-name
-  ;;                        temp-file
-  ;;                        (file-name-directory buffer-file-name))))
-  ;;     (list "ruby" (list "-c" local-file))))
-  ;; (push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
-  ;; (push '("\\(Rake\\|Cap\\)file$" flymake-ruby-init) flymake-allowed-file-name-masks)
-  ;; (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
-  ;; (add-hook 'ruby-mode-hook
-  ;;           '(lambda ()
-  ;;              ;; Don't want flymake mode for ruby regions in rhtml files
-  ;;              (if (not (null buffer-file-name)) (flymake-mode))))
+  (defun flymake-ruby-init ()
+    (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+                         'flymake-create-temp-inplace))
+           (local-file  (file-relative-name
+                         temp-file
+                         (file-name-directory buffer-file-name))))
+      (list "ruby" (list "-c" local-file))))
+  (push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
+  (push '("\\(Rake\\|Cap\\|Guard\\)file$" flymake-ruby-init) flymake-allowed-file-name-masks)
+  (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
+  (add-hook 'ruby-mode-hook
+            '(lambda ()
+               ;; Don't want flymake mode for ruby regions in rhtml files
+               (if (not (null buffer-file-name)) (flymake-mode))))
   (defun flymake-perl-init ()
     (let* ((temp-file (flymake-init-create-temp-buffer-copy
                        'flymake-create-temp-inplace))
