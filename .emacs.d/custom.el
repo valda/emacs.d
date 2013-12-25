96da7e63118f8c0038539abbb4448ab13f420d82
YAMAGUCHI Seiji <valda@underscore.jp>
2013-12-25 13:00:16 +0900

    iroiro

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,7 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((syntax . elisp)))))
+ '(rails-minor-mode-global-prefix-key "C-c C-c")
+ '(rails-minor-mode-local-prefix-key "C-c")
+ '(safe-local-variable-values (quote ((encoding . utf-8) (syntax . elisp))))
+ '(session-use-package t nil (session)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
