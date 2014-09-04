40d30f915677e21df4071021f5af81f1f3612d17
YAMAGUCHI Seiji <yamaguchi@hld.co.jp>
2014-09-04 17:58:33 +0900

    default options

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,6 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(git-gutter:window-width 0)
+ '(google-translate-default-source-language "en")
+ '(google-translate-default-target-language "ja")
+ '(magit-diff-options (quote ("--ignore-all-space" "--ignore-space-change")))
  '(safe-local-variable-values (quote ((encoding . utf-8) (syntax . elisp))))
  '(session-use-package t nil (session)))
 (custom-set-faces
